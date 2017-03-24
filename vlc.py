a69e5c3629cd78549dd93d6638edc98923d505bc
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-03-23 21:46:18 -0700

    connect db

--- vlc.py
+++ vlc.py
@@ -5,20 +5,38 @@ from flask_wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
 from wtforms.validators import InputRequired, Email, Length
 
-from flask_pymongo import PyMongo
+from flaskext.mysql import MySQL
 from bson import ObjectId
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
 
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/MathJoy'
-app.config['MONGO_DBNAME'] = 'MathJoy'
-
-mongo = PyMongo(app)
+app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
+app.config['MYSQL_DATABASE_PORT'] = 3306
+app.config['MYSQL_DATABASE_DB'] = 'vlc'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 
 Bootstrap(app)
 
+mysql = MySQL()
+mysql.init_app(app)
+
+
+@app.route('/db_test')
+def db_test():
+    conn = mysql.connect()
+    cur = conn.cursor()
+    data = None
+    try:
+        cur.execute("SELECT * FROM student_pool")
+        data = cur.fetchall()
+    except Exception as error:
+            print 'Read database failed: ', error
+    finally:
+            cur.close()
+    return render_template('vlc_db_test.html', title="DB Test", data=data)
 
 @app.route('/')
 def index():
